5f81e0118017d7ec07f26f0dedb698307d30ae87
valeriia <novikovavaleria994@gmail.com>
2025-06-23 14:14:04 +0200

    remove MainStoryboard

--- Mood Diary/AppDelegate.swift
+++ Mood Diary/AppDelegate.swift
@@ -7,15 +7,20 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+    var window: UIWindow?
+
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+            window = UIWindow()
+            window?.rootViewController = ViewController()
+            window?.makeKeyAndVisible()
+        
+            return true
+        }
 
     // MARK: UISceneSession Lifecycle
 
