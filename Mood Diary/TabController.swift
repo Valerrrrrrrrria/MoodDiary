17d31ff9d270f3e5567c60e627c0d20e825843c0
valeriia <novikovavaleria994@gmail.com>
2025-07-03 15:53:02 +0200

    tabbar customization

--- Mood Diary/TabController.swift
+++ Mood Diary/TabController.swift
@@ -11,26 +11,24 @@ class TabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tabBar.tintColor = .appAccent
-        tabBar.unselectedItemTintColor = UIColor.darkGray
+        tabBar.tintColor = .tabbarActive
+        tabBar.unselectedItemTintColor = .tabbarInactive
         tabBar.backgroundColor = UIColor.appBackground
         setupTabs()
     }
     
     private func setupTabs() {
-        let today = createNav(with: NSLocalizedString("1", comment: ""), and: UIImage.checkmark, vc: MainViewController())
-        let calendar = createNav(with: NSLocalizedString("2", comment: ""), and: UIImage.remove, vc: CalendarViewController())
-        let settings = createNav(with: NSLocalizedString("3", comment: ""), and: UIImage.actions, vc: SettingsViewController())
+        let today = createNav(with: NSLocalizedString("tabbar_today", comment: ""), and: UIImage.tabbarToday, vc: MainViewController())
+        let calendar = createNav(with: NSLocalizedString("tabbar_calendar", comment: ""), and: UIImage.tabbarCalendar, vc: CalendarViewController())
+        let settings = createNav(with: NSLocalizedString("tabbar_settings", comment: ""), and: UIImage.tabbarSettings, vc: SettingsViewController())
         
         self.setViewControllers([today, calendar, settings], animated: true)
     }
     
     private func createNav(with title: String, and image: UIImage?, vc: UIViewController) -> UINavigationController {
         let nav = UINavigationController(rootViewController: vc)
-        
         nav.tabBarItem.title = title
         nav.tabBarItem.image = image
-        
         return nav
     }
 }
